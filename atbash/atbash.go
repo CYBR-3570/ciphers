c09c906311a6c2f6079af2eecb5ffad0a2f5526a
Matthew Faltys <mfaltys@gmail.com>
2018-08-22 20:15:46 -0500

    Add file flag

--- atbash/atbash.go
+++ atbash/atbash.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 )
 
 func main() {
+	// read in runtime flags
+	filePtr := flag.String("file", "text", "relative path to text file")
+	flag.Parse()
+
 	// read in text file
-	dat, err := ioutil.ReadFile("text")
+	dat, err := ioutil.ReadFile(*filePtr)
 	if err != nil {
 		fmt.Println("could not read in input text file")
 	}
 
+	// print current file contents
 	fmt.Print(string(dat))
+
+	// set dictionary aka the reverse of the english alphabet
 	dictionary := []string{"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"}
 
+	// loop through text string
 	for _, x := range dat {
+		// convert ascii to 0-26 aka alphabet
 		a := x - 65
+
+		// compute for every character
 		switch {
 		case (a >= 0) && (a <= 26):
 			// convert
---
+++ atbash/readme.md
@@ -0,0 +1,5 @@
+# usage
+`go run atbash`: will run atbash against the file 'text'  
+`go run atbash -file filenamehere`: will run atbash against the contents of the
+file './filenamehere'  
+`go build atbash` will compile atbash into a dynamic executable
