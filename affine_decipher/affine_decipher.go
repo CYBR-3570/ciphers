e1073dc20975af7408593a3f2b928621872d690c
Matthew Faltys <mfaltys@gmail.com>
2018-09-05 16:24:30 -0500

    Finish decipher program

--- affine_decipher/affine_decipher.go
+++ affine_decipher/affine_decipher.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"math/big"
+	"strconv"
 )
 
 func main() {
@@ -24,7 +26,6 @@ func main() {
 	fmt.Println("")
 
 	// set dictionary aka the reverse of the english alphabet
-	//dictionary := []string{"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"}
 	dictionary := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
 	// loop through text string
@@ -32,13 +33,22 @@ func main() {
 		// convert ascii to 0-26 aka alphabet
 		a := x - 65
 
+		// calculate modular inverse
+		inverse := new(big.Int).ModInverse(big.NewInt(int64(*magnitudePtr)), big.NewInt(26))
+		s := fmt.Sprintf("%d", inverse)
+		i, _ := strconv.Atoi(s)
+
 		// compute for every character
 		switch {
 		case (a >= 0) && (a <= 26):
-			// convert
-			b := ((*magnitudePtr * int(a)) + *shiftPtr) % 26
+
+			// D(y) = c(y - b) mod26
+			b := (i * (int(a) - *shiftPtr)) % 26
+			// make the mod remainder if negative
+			if b < 0 {
+				b = b + 26
+			}
 			fmt.Printf("%s", dictionary[b])
-			//fmt.Printf("%s", dictionary[a])
 		case a == 223:
 			// space character
 			fmt.Printf(" ")
