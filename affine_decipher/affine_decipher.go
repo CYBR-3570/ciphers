3ed5e5b7d09c904f4ab332a698b3c451e010b48e
Matthew Faltys <mfaltys@gmail.com>
2018-09-05 10:05:43 -0500

    Add initial decipher

---
+++ affine_decipher/affine_decipher.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	// read in runtime flags
+	filePtr := flag.String("file", "text1", "relative path to text file")
+	magnitudePtr := flag.Int("magnitude", 11, "magnitude of cipher aka the '11' in '(11x+8) MOD 26'")
+	shiftPtr := flag.Int("shift", 8, "shift of the cipher aka the '8' in '(11x+8) MOD 26'")
+	flag.Parse()
+
+	// read in text file
+	dat, err := ioutil.ReadFile(*filePtr)
+	if err != nil {
+		fmt.Println("could not read in input text file")
+	}
+
+	// print current file contents
+	fmt.Print(string(dat))
+	fmt.Println("")
+
+	// set dictionary aka the reverse of the english alphabet
+	//dictionary := []string{"Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"}
+	dictionary := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+	// loop through text string
+	for _, x := range dat {
+		// convert ascii to 0-26 aka alphabet
+		a := x - 65
+
+		// compute for every character
+		switch {
+		case (a >= 0) && (a <= 26):
+			// convert
+			b := ((*magnitudePtr * int(a)) + *shiftPtr) % 26
+			fmt.Printf("%s", dictionary[b])
+			//fmt.Printf("%s", dictionary[a])
+		case a == 223:
+			// space character
+			fmt.Printf(" ")
+		case a == 201:
+			// end of line
+			fmt.Printf("\n")
+		default:
+		}
+	}
+}
---
+++ affine_decipher/readme.md
@@ -0,0 +1,3 @@
+# usage
+`go run affine_decipher --file text1 --magnitude 6 --shift 2`: will run affine_decipher against the file 'text1' with the function 6x+2 MOD 26  
+`go build affine_decipher` will compile affine_decipher into a dynamic executable
